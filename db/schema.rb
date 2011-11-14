edf74d44adaabc5ee1dc5db14544efccc562303d
Bflatau <bflatau@tulane.edu>
2011-11-13 19:34:42 -0800

    finished chap 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114024701) do
+ActiveRecord::Schema.define(:version => 20111114031449) do
 
   create_table "cars", :force => true do |t|
     t.datetime "created_at"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20111114024701) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
